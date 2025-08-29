3d727dc8e613abc8878574cdb445ca1600aac6a7
Arslan Ali <iarslanaly@yahoo.com>
2025-08-29 14:13:32 +0500

    introductin section

---
+++ lib/presentation/portfolio/portfolio.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class PortfolioPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Portfolio')),
+      body: Center(child: Text('Portfolio Page Content')),
+    );
+  }
+}
